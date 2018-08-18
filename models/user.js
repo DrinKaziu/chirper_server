59b7462553595f3b016bc4435d311cda49b50908
SnowSurfer <drinkaziu@gmail.com>
2018-08-18 14:22:03 -0400

    add bcrypt to user model

--- models/user.js
+++ models/user.js
@@ -21,6 +21,29 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+//hash the password before saving
+userSchema.pre("save", async function(next) {
+  try {
+    if(!this.isModified("password")) {   //if the password hasn't been changed, don't hash it again
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch(err) {
+    return next(err); //when we pass err to next it goes to our error handler
+  }
+});
+
+userSchema.method.comparePassword = async function(inputPassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(inputPassword, this.password); //compare if user input matches saved password
+      return isMatch; //if isMatch = true - login successful
+  } catch(err) {
+    return next(err)
+  }
+}
+
 const User = mongoose.model("User", userSchema);
 
-model.export = User; 
+model.export = User;
