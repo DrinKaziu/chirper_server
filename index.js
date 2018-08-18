148e064d308736d06ae7a548b20d4be1b97d6d5b
SnowSurfer <drinkaziu@gmail.com>
2018-08-18 16:33:59 -0400

    create messages handler and route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
 
 
 const PORT = 3001;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", messagesRoutes);
 
 //Routes will go here...
 
